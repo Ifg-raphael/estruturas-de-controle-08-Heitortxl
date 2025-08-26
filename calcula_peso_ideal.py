63248bf80dc7814d5bb7aac286a09eee27d0dd85
nacckl <nacckleial@gmail.com>
2025-08-26 15:44:04 -0300

    Exercício 8 - Heitor

--- calcula_peso_ideal.py
+++ calcula_peso_ideal.py
@@ -1,1 +1,11 @@
 # Sua solução aqui
+altura = float(input())
+sexo = input().strip().upper()
+
+if sexo == "M":
+    pesoideal = (72.7*altura)-58
+elif sexo == "F":
+   pesoideal = (62.1*altura)-44.7
+
+
+print(f"peso_ideal:{pesoideal:.2f}")
